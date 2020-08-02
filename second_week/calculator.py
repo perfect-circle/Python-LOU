70a80b0ad793b27b7d983506778c0760cffaf678
perfect-circle <1602593812@qq.com>
2020-08-02 18:54:19 +0800

    工资计算器读写数据文件

--- second_week/calculator.py
+++ second_week/calculator.py
@@ -1,198 +1,205 @@
 #!/usr/bin/env python3
-
-import sys
 import csv
+import sys
 from collections import namedtuple
 
-# 税表条目类，该类由namedtuple动态创建，代表一个命名元组
-IncomeTaxQuickLookupItem = (
-        'IncomTaxQuickLookupItem',
-        ['start_point', 'tax_rate', 'quick_subtractor']
-        )
+# 税率表条目类，该类由namedtuple动态创建，代表一个命名元组：
+IncomeTaxQuickLookupItem = namedtuple(
+        'IncomeTaxQuickLookupItem',
+        ['taxable','taxrate','quick'])
 
+# 起征点
+CUTOFF_POINT = 5000
 
-# 起征点常量
-INCOME_TAX_START_POINT = 5000
-
-# 税率表，里面的元素类型为前面创建的IncomeTaxQuickLookupItem
+# 税率及速算扣除数对应表：
 INCOME_TAX_QUICK_LOOKUP_TABLE = [
         IncomeTaxQuickLookupItem(80000, 0.45, 15160),
         IncomeTaxQuickLookupItem(55000, 0.35, 7160),
-        IncomeTaxQuickLookupItem(35000, 0.3, 4410),
+        IncomeTaxQuickLookupItem(35000, 0.30, 4410),
         IncomeTaxQuickLookupItem(25000, 0.25, 2660),
-        IncomeTaxQuickLookupItem(12000, 0.2, 1410),
-        IncomeTaxQuickLookupItem(3000, 0.1, 210),
+        IncomeTaxQuickLookupItem(12000, 0.20, 1410),
+        IncomeTaxQuickLookupItem(3000, 0.10, 210),
         IncomeTaxQuickLookupItem(0, 0.03, 0)
-]
+        ]
 
 class Argv(object):
-    """提取命令"""
+    """命令采集类"""
     def __init__(self):
         self.args = sys.argv[1:]
 
-    def _read_command(self, option):
-        """输入参数，返回参数后的值"""
+    def _get_args(self,option):
+        """输入参数，返回参数后的命令"""
         try:
             index = self.args.index(option)
             return self.args[index + 1]
         except:
-            print('Parameter Error')
+            print("Option Error")
             exit()
 
     @property
     def config_path(self):
-        """返回配置文件路径"""
-        return self._read_command('-c')
+        """
+        TODO: Docstring for config_path.
+        """
+        return self._get_args('-c')
 
     @property
     def userdata_path(self):
-        """返回用户数据文件"""
-        return self._read_command('-d')
+        """
+        TODO: Docstring for userdata_path.
+        """
+        return self._get_args('-d')
 
     @property
     def export_path(self):
-        return self._read_command('-o')
+        """
+        TODO: Docstring for export_path.
+        """
+        return self._get_args('-o')
 
 args = Argv()
 
 class Config(object):
-    """提取配置文件信息"""
+
+    """配置文件获取类"""
 
     def __init__(self):
-        self.config = self._read_config
+        """TODO: to be defined. """
+        self.config = self._get_config()
 
-    def _read_config(self):
-        """读取配置文件"""
-
+    def _get_config(self):
+        """
+        TODO: Docstring for _get_config.
+        """
         config = {}
         with open(args.config_path) as f_ob:
             for line in f_ob.readlines():
                 key,value = line.strip().split('=')
-                try:
-                   config[key.strip()] = float(value.strip())
-                except:
-                    print("Parameter  Error")
-                    exit()
+                config[key.strip()] = float(value.strip())
 
-    def _get_config(self, key):
-        """获取配置文件"""
-        try:
-            return self.config[key]
-        except:
-            print("Config Error")
-            exit()
+        return config
 
     @property
-    def social_insurance_baseline_low(self):
-        return self._get_config('JiShuL')
+    def fund_low(self):
+        """
+        TODO: Docstring for fund_low.
+        """
+        return self.config['JiShuL']
 
     @property
-    def social_insurance_baseline_high(self):
-        return self._get_config('JiShuH')
+    def fund_high(self):
+        """
+        TODO: Docstring for fund_high.
+        """
+        return self.config['JiShuH']
 
     @property
-    def social_insurance_total(self):
+    def social_security_ratio(self):
+        """
+        TODO: Docstring for social_security_ratio.
+        """
         return sum([
-            self._get_config('YangLao'),
-            self._get_config('YiLiao'),
-            self._get_config('ShiYe'),
-            self._get_config('GongShang'),
-            self._get_config('ShengYu'),
-            self._get_config('GongJiJin')
-            ])
+                self.config['YangLao'],
+                self.config['YiLiao'],
+                self.config['ShiYe'],
+                self.config['GongShang'],
+                self.config['ShengYu'],
+                self.config['GongJiJin']
+                ])
 
 config = Config()
 
 class UserData(object):
-    """获取员工数据"""
+
+    """处理员工数据类"""
 
     def __init__(self):
-        self.userdata = self._read_users_data
+        """TODO: to be defined. """
+        self.userdata = self._get_userdata()
 
-    def _read_users_data(self):
-        """读取与员工数据"""
-        userdata = []
-        with open(args.userdata_path) as f_ob:
-            for line in f_ob.readlines():
-                id_user, income_string = line.strip().split(',')
-                try:
-                    income = int(income_string)
-                except:
-                    print("Parameter Error")
-                    exit()
-                userdata.append((id_user, income))
-        return userdata
+    def _get_userdata(self):
+        """
+        TODO: Docstring for _get_userdata.
+        """
+        try:
+            with open(args.userdata_path) as f_ob:
+                userdata = list(csv.reader(f_ob))
+            return userdata
+        except:
+            print("UserData File Error")
+            exit()
 
-    def __iter__(self):
-        return iter(self.userdata)
+class Calculator(object):
 
-class IncomeTaxCalculator(object):
-    """税后工资计算类"""
+    """计算结果类"""
 
     def __init__(self,userdata):
-        self.userdata = userdata
+        """
+        输入员工数据，返回计算结果
+        """
+        self._userdata = userdata
 
     @staticmethod
-    def calc_social_indurance_moeny(income):
-        """计算社保金额"""
-        if income < config.social_insurance_baseline_low:
-            return config.social_insurance_baseline_low * config.social_insurance_total
-
-        if income > config.social_insurance_baseline_high:
-            return config.social_insurande_baseline_high * config.social_insurance_total
+    def calculator_social_security(income):
+        """
+        计算社保金额
+        """
+        if income < config.fund_low:
+            return config.fund_low * config.social_security_ratio
+        elif income > config.fund_high:
+            return config.fund_high * config.social_security_ratio
         else:
-            return income * config.social_insurance_total
+            return income * config.social_security_ratio
 
     @classmethod
-    def calc_income_tax_and_remain(cla,income):
-        """计算税后工资"""
+    def tax(cls, income):
+        """
+        计算个税金额
+        """
+        taxable_income = income - cls.calculator_social_security(income) - CUTOFF_POINT
 
-        # 计算社保金额
-        social_insurance_money = cls.calc_social_insurance_money(income)
-
-        # 计算应纳税额
-        real_income = income - social_insurance_money
-        taxable_part = real_income - INCOME_TAX_START_POINT
-
-        # 从高到第判断落入的税率区间，如果找到则用该区间的参数计算纳税额并返回结果
         for item in INCOME_TAX_QUICK_LOOKUP_TABLE:
-            if taxable_part > item.start_point:
-                tax = taxable_part * item.tax_rate + item.quick_subtractor
-                return '{:.2f}'.format(tax), '{:.2f}'.format(real_income - tax)
+            if taxable_income > item.taxable:
+                return taxable_income * item.taxrate - item.quick
 
-        return '0.00', '{:.2f}'.format(real_income)
+        return 0.00
 
-    def calc_for_all_userdata(self):
-        """计算所有用户的税后工资"""
+    def payroll(self):
+        """计算出员工工资单"""
         result = []
-        for id_user, income in self.userdata:
-            # 计算社保金额
-            social_insurance_money = '{:.2f}'.format(
-                    self.calc_social_insurance_money(income))
-
-            # 计算税后工资
-            tax, remain = self.calc_income_tax_and_remain(income)
-
-            # 添加到结果集
-            result.append([id_user, income, social_insurance_money, tax, remain])
-
+        for employee in self._userdata:
+            employee_id, income = employee
+            income = float(income)
+            social_security = Calculator.calculator_social_security(income)
+            tax = Calculator.tax(income)
+            real_income = income - social_security - tax
+            result.append([employee_id, '{:.2f}'.format(income), '{:.2f}'.format(social_security), '{:.2f}'.format(tax), '{:.2f}'.format(real_income)])
         return result
 
-    def export(self):
-        """导出所有用户"""
+class Export(object):
 
-        # 计算所有用户的税后工资
-        result = self.calc_for_all_userdata()
+    """输出到文件类"""
 
-        with open(args.export_path, 'w', newline='') as f_ob:
-            # 创建CSV文件写入对象
-            writer = csv.writer(f_ob)
-            # 写入多行
-            writer.writerows(result)
+    def __init__(self, result):
+        self._result = result
 
-if __name__ == "__main__":
-    # 创建税后工资计算器
-    calculator = IncomeTaxCalculator(UserData())
+    def export_file(self):
+        """
+        以CSV格式输入到目标文件
+        """
+        with open(args.export_path,'w') as f_ob:
+            csv.writer(f_ob).writerows(self._result)
 
-    # 调用export方法导出税后工资到文件
-    calculator.export()
+if __name__ == '__main__':
+
+    # 获取员工数据
+    data = UserData()
+    data_list = data.userdata
+
+    # 获取计算结果
+    calculator = Calculator(data_list)
+    result = calculator.payroll()
+
+    # 输出到目标文件
+    export = Export(result)
+    export.export_file()

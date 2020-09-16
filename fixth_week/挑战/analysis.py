86d86e53fcb2f1f6772aff71ebfc7f15e82abee3
circle <circle@MacBook-Pro.lan>
2020-09-16 22:04:54 +0800

    复习数据分析后完成题目

--- fixth_week/挑战/analysis.py
+++ fixth_week/挑战/analysis.py
@@ -1,18 +1,20 @@
+import json
 import pandas as pd
 
-def analysis(file, user_id):
+def analysis(file,user_id):
     times = 0
     minutes = 0
 
     with open(file) as f:
-        data = pd.read_json(f)
+        df = pd.read_json(f)
 
-    times = data[data['user_id'] == user_id]['minutes'].size
-    minutes = data[data['user_id'] == user_id]['minutes'].sum()
+    times = df[df['user_id'] == user_id]['course'].count()
+    minutes = df[df['user_id'] == user_id]['minutes'].sum()
 
     return times, minutes
 
 if __name__ == "__main__":
-    times, minutes = analysis('user_study.json', 199071)
-    print(times)
-    print(minutes)
+    file_name = input("Please entry file name: ")
+    user_id = int(input("Please entry user ID: "))
+    times, minutes = analysis(file_name, user_id)
+    print("学习次数：{} \n学习总时间：{}".format(times,minutes))
